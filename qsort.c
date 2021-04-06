11f063938981fdd1e5957e9b8e34052d6bbbe413
PIPIPINoBrain <70845572+PIPIPINoBrain@users.noreply.github.com>
2021-04-06 21:44:35 +0800

    Create qsort.c

---
+++ qsort.c
@@ -0,0 +1,31 @@
+#void* 指针可以接收任意类型指针
+#void* 指针不能解应用，也不能加地址
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+#include<stdlib.h>
+
+//void qsort(void* base,
+//	size_t num,
+//	size_t width,
+//	int(*cmp)(const void *e1,const void *e2)
+//)
+
+int cmp_int(const void* e1, const void* e2)
+{
+	return *(int*)e1 - *(int*)e2;
+
+}
+//第一个元素小于第二个返回小于0的数，等于返回0，大于返回大于0的数
+
+int main()
+{
+	int arr[10] = { 1,3,5,7,9,2,4,6,8,10 };
+	int sz = sizeof(arr) / sizeof(arr[0]);
+	qsort(arr,sz,sizeof(arr[0]),cmp_int);
+	int i = 0;
+	for (i = 0; i < sz; i++)
+	{
+		printf("%d ", arr[i]);
+	}
+	return 0;
+}
