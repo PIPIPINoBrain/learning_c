8739bcc8e3b1a2d31662619b88778ed72348ac1a
PIPIPINoBrain <70845572+PIPIPINoBrain@users.noreply.github.com>
2021-04-18 18:42:31 +0800

    Update struct_three.c

--- struct_three.c
+++ struct_three.c
@@ -1,19 +1,14 @@
-#define _CRT_SECURE_NO_WARNINGS
-#include<stdio.h>
-#include<stdlib.h>
-#include<string.h>
-#include<assert.h>
-
-#pragma pack(1)
+//#pragma pack(1)  默认对其数修改（默认是8）  注意struct内存
 struct s1 {
 	int s;
 	char l;
 	char p;
 };
-#pragma pack()
+//#pragma pack()
 int main()
 {
 	struct s1 s1;
 	printf("%d\n", sizeof(s1));
+	printf("%d\n", offsetof(struct s1, l));//求相对偏移量，宏
 	return 0;
 }
